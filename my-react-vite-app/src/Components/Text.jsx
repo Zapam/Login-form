9856b328c6f6203ed831450813b69e54ed6f3b37
AmirZapam <amirzapam9@gmail.com>
2023-08-24 00:30:03 +0330

    Starting over

--- my-react-vite-app/src/Components/Text.jsx
+++ my-react-vite-app/src/Components/Text.jsx
@@ -1,13 +1,17 @@
-import './text.css'
+import "./text.css";
 export default function Text() {
   return (
     <div className="text-white flex flex-col items-center justify-center title h-screen">
       <div>
-        <h1 className="font-bold text-4xl heading md:w-2/5 text">Learn to code by<br />watching others</h1>
-        <p className="w-full md:w-2/5 mt-5 text paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Saepe nemo, iusto hic sit soluta cupiditate magni porro,
-          similique, nisi amet provident. vitae!
+        <h1 className="">
+          Learn to code by
+          <br />
+          watching others
+        </h1>
+        <p className="">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nemo,
+          iusto hic sit soluta cupiditate magni porro, similique, nisi amet
+          provident. vitae!
         </p>
       </div>
     </div>
